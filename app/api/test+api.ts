93264da51395497c3eb19650b183b31ca916973a
violettebunny <52981973+violettebunny@users.noreply.github.com>
2024-02-23 17:25:26 -0500

    basic firebase setup

--- app/api/test+api.ts
+++ app/api/test+api.ts
@@ -1,7 +1,6 @@
 import { ExpoRequest, ExpoResponse } from 'expo-router/server';
 
 export function GET(request: ExpoRequest) {
-  console.log('HIT');
   return ExpoResponse.json({
     message: 'Hello, world!',
   });
---
+++ firebase.config.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+import { getAuth } from 'firebase/auth';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyBEQclynG09XMRaH2hCZTxKy8TQmzcy9yo',
+  authDomain: 'rpg-stepper.firebaseapp.com',
+  projectId: 'rpg-stepper',
+  storageBucket: 'rpg-stepper.appspot.com',
+  messagingSenderId: '644378735917',
+  appId: '1:644378735917:web:53512d6387fdf6af124fa4',
+  measurementId: 'G-446VRF4TN0',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const analytics = getAnalytics(app);

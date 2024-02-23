de5b5e4b327c99ce2ba7720c60037cdbb9103c2c
violettebunny <52981973+violettebunny@users.noreply.github.com>
2024-02-22 20:58:16 -0500

    setup with typescript tailwind and expo-router

---
+++ app/(tabs)/two.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from 'react-native';
+
+export default function TabTwoScreen() {
+  return (
+    <View>
+      <Text>Tab Two</Text>
+      <View />
+    </View>
+  );
+}
---
+++ app/modal.tsx
@@ -0,0 +1,16 @@
+import { StatusBar } from 'expo-status-bar';
+import { Platform, StyleSheet } from 'react-native';
+
+import { Text, View } from 'react-native';
+
+export default function ModalScreen() {
+  return (
+    <View>
+      <Text>Modal</Text>
+      <View />
+
+      {/* Use a light status bar on iOS to account for the black space above the modal */}
+      <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
+    </View>
+  );
+}

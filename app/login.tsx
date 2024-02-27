6ac6f47f99b24ad44a002e540f9bf2cce67326ec
violettebunny <52981973+violettebunny@users.noreply.github.com>
2024-02-27 13:42:35 -0500

    debugging auth providers

--- app/login.tsx
+++
@@ -1,9 +0,0 @@
-const Login = () => {
-  return (
-    <form>
-      <input type="text" placeholder="Username" />
-      <input type="password" placeholder="Password" />
-      <button type="submit">Login</button>
-    </form>
-  );
-};

9a39ebd54dbb9e79e0e236f41a423a868ea29b28
violettebunny <52981973+violettebunny@users.noreply.github.com>
2024-02-29 16:34:39 -0500

    secrects protect

--- app.config.js
+++ app.config.js
@@ -32,6 +32,21 @@ export default {
       output: 'server',
     },
     plugins: ['expo-router'],
+    extra: {
+      firebase: {
+        apiKey: process.env.FIREBASE_API_KEY,
+        authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+        messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+        appId: process.env.FIREBASE_APP_ID,
+        measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+      },
+      googleSignIn: {
+        iosClientId: process.env.GOOGLE_SIGNIN_IOS_CLIENT_ID,
+        webClientId: process.env.GOOGLE_SIGNIN_WEB_CLIENT_ID,
+      },
+    },
   },
 };
 
--- firebase.config.js
+++ firebase.config.js
@@ -1,9 +1,9 @@
 // Import the functions you need from the SDKs you need
-import 'dotenv/config';
 import { initializeApp } from 'firebase/app';
 import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
+import Constants from 'expo-constants';
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -11,13 +11,13 @@ import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+  apiKey: Constants.expoConfig.extra.firebase.apiKey,
+  authDomain: Constants.expoConfig.extra.firebase.authDomain,
+  projectId: Constants.expoConfig.extra.firebase.projectId,
+  storageBucket: Constants.expoConfig.extra.firebase.storageBucket,
+  messagingSenderId: Constants.expoConfig.extra.firebase.messagingSenderId,
+  appId: Constants.expoConfig.extra.firebase.appId,
+  measurementId: Constants.expoConfig.extra.firebase.measurementId,
 };
 
 // Initialize Firebase
